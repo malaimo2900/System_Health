7666498797f36c96741a000dd1c43dc75b1f464e
Michael Alaimo <michaelalaimo@gmail.com>
2013-12-22 23:35:55 +0000

    The basis for a service configuratin per request

---
+++ REST_Service/protected/components/ServiceConfig/ServiceConfig.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class ServiceConfig extends CApplicationComponent {
+	private $outputAccept;
+	
+
+	public function init() {
+		$this->processHeaders();
+	}
+	
+	
+	/**
+	 * Will process the header information pertinent to the request
+	 * configuration
+	 */
+	private function processHeaders() {
+		$this->outputAccept;	
+	}
+	
+	
+}
