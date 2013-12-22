976f9fc76fa72252456b8d0e7bf196f5a3c3dc50
Michael Alaimo <michaelalaimo@gmail.com>
2013-12-22 22:22:35 +0000

    bug3

--- REST_Service/protected/components/Controller.php
+++ REST_Service/protected/components/Controller.php
@@ -20,7 +20,7 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
-	 
+	
 	 
 	/**
 	 * handle all init functionality.
