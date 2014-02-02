c851ab73e1342b0493979ec4e946734f5a9d2c22
Michael Alaimo <malaimo923@gmail.com>
2014-02-02 19:44:04 +0000

    Add support for better namespacing.

--- System_Process/local/autoload.php
+++ System_Process/local/autoload.php
@@ -2,7 +2,9 @@
 
 spl_autoload_register(function($class) {
 	$classParts = explode('\\', $class);
-	$classFile = __DIR__.'/class/'.$classParts[1].'.php';
+	unset($classParts[0]);
+	$classFile = __DIR__.'/class/'.implode('/', $classParts).'.php';
+	var_dump($classFile);
 	if (file_exists($classFile)) {
 		require($classFile);
 	}
